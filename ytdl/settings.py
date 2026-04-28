5782b27aa9b78edf43c5778ccad6fb3d2a827f87
Miguel Becker <habranea@disroot.org>
2026-04-28 15:17:12 -0300

    adding corsheaders (for health)

--- ytdl/settings.py
+++ ytdl/settings.py
@@ -25,7 +25,10 @@ STORAGES = {
     },
 }
 
+CORS_ALLOW_ALL_ORIGINS = True  
+
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
